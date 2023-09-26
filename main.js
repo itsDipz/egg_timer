335009ff9e3433667600aed44c4ed0208eb6af2c
itsDipz <131451520+itsDipz@users.noreply.github.com>
2023-09-26 13:53:36 +0200

    uppdate

--- main.js
+++ main.js
@@ -1,25 +1,11 @@
-function egg_timer(how_many_minutes){
-    let minute_in_miliseconds = 60000;
-    let total_miliseconds = (minute_in_miliseconds * how_many_minutes);
-
-    
-    let seconds = Math.floor(total_miliseconds / 1000);
-    let minutes = Math.floor(seconds / 60);
-    let remainingSeconds = seconds % 60;
-    
-    let interval_id = setInterval(() =>{
-        total_miliseconds = total_miliseconds - 1000;
-        seconds = Math.floor(total_miliseconds / 1000);
-        minutes = Math.floor(seconds / 60);
-        remainingSeconds = seconds % 60;
-        console.log(seconds)
-
-        document.querySelector(".timer_display").innerHTML = `${minutes},${remainingSeconds}`
-        if(seconds === 0){
-            console.log("Timer done");
-            clearInterval(interval_id)
-        }
-    },1000)
+function drag_dot(){
+    let the_dot = document.querySelector(".scroll_dot")
+    the_dot.addEventListener("drag", (event) => {
+        console.log(event.clientX)
+        console.log(event.clientY)
+    })
+    the_dot.addEventListener("dragend", (event) => {
+        
+    })
 }
-
-egg_timer(3);
+drag_dot()
