744ac18adbfa41bc4e52000fa27a83e7ebda69d2
itsDipz <131451520+itsDipz@users.noreply.github.com>
2023-09-26 14:08:13 +0200

    uppdate

--- main.js
+++ main.js
@@ -22,4 +22,19 @@ function egg_timer(how_many_minutes){
     },1000)
 }
 
+function slider(){
+    
+    const numericInput = document.querySelector(".timer_show");
+    const numericSlider = document.getElementById("numeric-slider");
+    window.addEventListener("DOMContentLoaded", () =>{
+        numericSlider.value = 600;
+        numericInput.innerHTML = 6.0;
+    })
+    numericSlider.addEventListener("input", () => {
+      numericInput.innerHTML = numericSlider.value / 100;
+      console.log(numericInput.value)
+    });
+}
+slider()
+
 egg_timer(3);
