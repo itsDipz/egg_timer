71abd291e425ce3839710ea060498e19d921fb88
itsDipz <131451520+itsDipz@users.noreply.github.com>
2023-10-01 22:54:35 +0200

    uppdate

--- main.js
+++ main.js
@@ -13,7 +13,13 @@ const colors = [
   "#fed559",
 ];
 
+
+
+
 function egg_timer(how_many_minutes) {
+
+  document.querySelector("#stop_button").removeEventListener("click", start_function1)
+
   let minute_in_miliseconds = 60000;
   let total_miliseconds = minute_in_miliseconds * how_many_minutes;
   console.log(total_miliseconds);
@@ -22,15 +28,24 @@ function egg_timer(how_many_minutes) {
   let minutes = Math.floor(seconds / 60);
   let remainingSeconds = seconds % 60;
 
+
+
+  let stop_button = document.querySelector("#stop_button");
+
   let interval_id = setInterval(() => {
+
     total_miliseconds = total_miliseconds - 1000;
     seconds = Math.floor(total_miliseconds / 1000);
     minutes = Math.floor(seconds / 60);
     remainingSeconds = seconds % 60;
     console.log(seconds);
-
+    if(window.localStorage.getItem("seconds") !== null){
+      window.localStorage.removeItem("seconds")
+    }
+    window.localStorage.setItem("seconds", seconds);
+    console.log(window.localStorage)
     document.querySelector(
-      ".timer_display"
+      ".timer_show"
     ).innerHTML = `${minutes}:${remainingSeconds}`;
     function hasTwoDigits(integer) {
       const integerStr = Math.abs(integer).toString(); // Convert to a string
@@ -39,7 +54,7 @@ function egg_timer(how_many_minutes) {
 
     if (hasTwoDigits(remainingSeconds) === false) {
       document.querySelector(
-        ".timer_display"
+        ".timer_show"
       ).innerHTML = `${minutes}:0${remainingSeconds}`;
     }
 
@@ -48,7 +63,16 @@ function egg_timer(how_many_minutes) {
       clearInterval(interval_id);
     }
   }, 1000);
+  
+  window.localStorage.setItem("interval_id", interval_id);
+
+  if(document.querySelector("#stop_button") !== null){
+    stop_button.addEventListener("click", stop_function)
+  }
 }
+
+
+
 
 function slider() {
   const numericInput = document.querySelector(".timer_show");
@@ -69,18 +93,40 @@ function slider() {
     numericInput.innerHTML = numericSlider.value / 100;
     const inputString = numericInput.innerHTML;
     const floatValue = parseFloat(inputString);
-    numericInput.innerHTML = floatValue; // 3.25
+    numericInput.innerHTML = floatValue; 
   });
 
   let button = document.querySelector("button");
-  button.addEventListener("click", () => {
-    egg_timer(parseFloat(document.querySelector(".timer_show").innerHTML));
-    document.querySelector(".timer_show").remove();
-    let timer_display = document.createElement("div");
-    timer_display.classList.add("timer_display");
-    document.querySelector(".container").appendChild(timer_display);
-    document.querySelector("input").remove();
-    document.querySelector("button").remove();
-  });
+  console.log(button)
+  button.addEventListener("click", start_function1);
 }
+
+function start_function1(){
+  if(document.querySelector("input") !== null){
+    document.querySelector("input").remove();
+  }
+  
+  document.querySelector("button").id = "stop_button";
+  document.querySelector("button").innerHTML = "Stop"
+  egg_timer(parseFloat(document.querySelector(".timer_show").innerHTML));
+}
+
+function stop_function(){
+  let interval_id = window.localStorage.getItem("interval_id");
+  clearInterval(interval_id)
+  window.localStorage.clear();
+  document.querySelector("#stop_button").removeEventListener("click", stop_function)
+  document.querySelector("button").id = "";
+  document.querySelector("button").innerHTML = "start";
+  document.querySelector("button").addEventListener("click", start_function1)
+}
+
+function convert_seconds_to_minutes(seconds){
+  let minutes = Math.floor(seconds / 60);
+  let remainingSeconds = seconds % 60;
+  let the_return_minutes = parseFloat(`${minutes}.${remainingSeconds}`);
+  return the_return_minutes;
+}
+
+
 slider();
