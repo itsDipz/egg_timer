579ba1c58e18e31134ab0037d5c33d04dfca8d13
itsDipz <131451520+itsDipz@users.noreply.github.com>
2023-10-03 14:46:15 +0200

    uppdate

--- utils.js
+++ utils.js
@@ -26,8 +26,8 @@ export function create_scroll(){
         let temp2 = array_of_defaults[1];
         let temp3 = array_of_defaults[2];
         array_of_defaults[0] = temp2;
-        array_of_defaults[2] =  temp3;
-        array_of_defaults[1] = temp1;
+        array_of_defaults[1] =  temp3;
+        array_of_defaults[2] = temp1;
         
         let all_nodes_selected = document.querySelectorAll(".selected");
         let all_nodes_not_selected = document.querySelectorAll(".not_selected");
@@ -47,6 +47,66 @@ export function create_scroll(){
         options_container.innerHTML = "";
         for (let index = 0; index < array_of_defaults.length; index++) {
             options_container.appendChild(array_of_defaults[index]);
+        }
+
+        switch(document.querySelector(".selected").innerHTML){
+            case "Hard boiled":
+                document.querySelector(".timer_show").innerHTML = "11:00";
+                document.querySelector("input").value = 1100;
+            break;
+            case "Default boiled":
+                document.querySelector(".timer_show").innerHTML = "6:00";
+                document.querySelector("input").value = 600;
+            break;
+            case "Soft boiled":
+                document.querySelector(".timer_show").innerHTML = "2:00";
+                document.querySelector("input").value = 200;
+            break;
+        }
+    })
+
+
+    document.querySelector(".front_arrow").addEventListener("click", () =>{
+        let temp1 = array_of_defaults[0];
+        let temp2 = array_of_defaults[1];
+        let temp3 = array_of_defaults[2];
+        array_of_defaults[0] = temp3;
+        array_of_defaults[1] =  temp1;
+        array_of_defaults[2] = temp2;
+        
+        let all_nodes_selected = document.querySelectorAll(".selected");
+        let all_nodes_not_selected = document.querySelectorAll(".not_selected");
+        all_nodes_selected.forEach((index) => {
+            index.classList.remove("selected");
+        })
+        all_nodes_not_selected.forEach((index) => {
+            index.classList.remove("not_selected");
+        })
+
+
+        array_of_defaults[0].classList.add("not_selected");
+        array_of_defaults[1].classList.add("selected");
+        array_of_defaults[2].classList.add("not_selected");
+
+        let options_container = document.querySelector(".options_container");
+        options_container.innerHTML = "";
+        for (let index = 0; index < array_of_defaults.length; index++) {
+            options_container.appendChild(array_of_defaults[index]);
+        }
+
+        switch(document.querySelector(".selected").innerHTML){
+            case "Hard boiled":
+                document.querySelector(".timer_show").innerHTML = "11:00";
+                document.querySelector("input").value = 1100;
+            break;
+            case "Default boiled":
+                document.querySelector(".timer_show").innerHTML = "6:00";
+                document.querySelector("input").value = 600;
+            break;
+            case "Soft boiled":
+                document.querySelector(".timer_show").innerHTML = "2:00";
+                document.querySelector("input").value = 200;
+            break;
         }
     })
 }
